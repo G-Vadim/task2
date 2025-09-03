985464c6da13c5a8777c6e934bb9fab884beb29e
G-Vadim <vadimgodonoga9@gmail.com>
2025-09-03 18:22:30 +0300

    Add new feature

--- task1.c
+++ task1.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 void papa(){
-
     printf("goodbye");
-
 }
 
 int main() {
     printf("Salut, lume!\n");
-
     papa();
-
+    printf("\nAcesta este un mesaj nou!\n");
     return 0;
 }
